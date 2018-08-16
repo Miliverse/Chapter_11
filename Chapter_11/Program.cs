0479e479b2c0ba0797808fb0c96492b0b6555c4a
Miliverse <procrastinatorhal23@gmail.com>
2018-08-16 17:27:38 +0400

    Set up basic framework relating to editing external TXT files. Unfinished.

--- Chapter_11/Program.cs
+++ Chapter_11/Program.cs
@@ -14,7 +14,7 @@ namespace Chapter_11
     public static class Game
     {
         //Establish typewriting effect
-        static void Typewrite(string message)
+        public static void Typewrite(string message)
         {
             for (int i = 0; i < message.Length; i++)
             {
@@ -29,6 +29,7 @@ namespace Chapter_11
         public static void StartGame()
         {
             Console.ForegroundColor = ConsoleColor.White;
+            Console.Title = "Chapter 11";
             OpeningLore();
         }
 
@@ -59,16 +60,107 @@ namespace Chapter_11
         private const int MINIMIZE = 6;
         private const int RESTORE = 9;
 
-        static void Main(string[] args)
+        static void Main(string[] args) // program game running thingy. stick stuff you want the program to run at launch in here
         {
             //I'm not commenting this, figure it out
             Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
             ShowWindow(ThisConsole, MAXIMIZE);
             Game.StartGame(); //Start the game
+            ExternalData.Menu();
+        }
+    }
+    class ExternalData // External Data Reading, I'm going to comment none of this
+    {
+        public static string DataFile = "data.txt";
+        public static string Content = "(Empty File)";
+        public static string Input = "";
+        public static bool Run = true;
+        public static int Choice = 0;
+
+        static void ColorText(string Message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(Message);
+            Console.ResetColor();
 
         }
 
-   
+        static void Placeholder()
+        {
+            Console.Title = "Chapter 11 - Save Mode";
+
+            while (Run == true)
+            {
+                Menu();
+            }
+        }
+
+        public static void Menu()
+        {
+            string input = "";
+            int Choice = 0;
+
+            Console.WriteLine("\n\n\n\n\n------------------------------------------");
+            Console.WriteLine(" 1) Read Content 2) Update Content 3) Exit");
+            Console.WriteLine("------------------------------------------");
+
+            Input = Console.ReadLine();
+            Choice = Convert.ToInt32(Input);
+            if (Choice == 1)
+            {
+                Console.Clear();
+                if (File.Exists(DataFile))
+                {
+                    ColorText("znznThe File contents are: ");
+                    Content = File.ReadAllText(DataFile);
+                }
+                Console.WriteLine(Content);
+            }
+            else if (Choice == 2)
+            {
+                Console.Clear();
+                ColorText("\n\nEnter new content and press enter to save it:");
+                Input = Console.ReadLine();
+                Content = Input;
+
+                ColorText("\nConfirm that file contents will be replaced.\nType Y or N:");
+
+                Input = Console.ReadLine();
+                Input = Input.ToLower();
+
+                if (Input == "y")
+                {
+                    File.WriteAllText(DataFile, Content);
+                    ColorText("File updated.");
+                }
+                else
+                {
+                    ColorText("File not updated.")
+                }
+            }
+            
+        }
     }
-    
 }
+
+            
+            
+                    
+
+
+
+
+
+
+                
+            
+           
+
+        
+
+        
+
+    
+    
+
